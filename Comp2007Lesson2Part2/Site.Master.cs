427d74755528c9a7b85464d5055ed018f3b85ee2
patr9240 <patr9240@gmail.com>
2016-05-19 19:03:47 -0400

    added addActiveClass method to site.master.cs

--- Comp2007Lesson2Part2/Site.Master.cs
+++ Comp2007Lesson2Part2/Site.Master.cs
@@ -11,7 +11,36 @@ namespace Comp2007Lesson2Part2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            addActiveClass();
 
+        }
+
+        /**
+         * This method adds class of "active" to list items in the main navbar depending on the page title
+         * 
+         * @method addActiveClass
+         * @return{void}
+         */
+        private void addActiveClass()
+        {
+            switch (Page.Title)
+            {
+                case "Home":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+                case "Products":
+                    products.Attributes.Add("class", "active");
+                    break;
+                case "About":
+                    about.Attributes.Add("class", "active");
+                    break;
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+            }
         }
     }
 }
